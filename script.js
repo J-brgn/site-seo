793ea0fd76434153da5a47ee71198b454bcd0741
Julien Borgeon <136490070+J-brgn@users.noreply.github.com>
2024-02-22 14:56:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -66,3 +66,11 @@ $(document).ready(function() {
         console.log("Le copier-coller est désactivé sur ce site.");
     });
 });
+
+window.addEventListener('scroll', function() {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.querySelector('.progress-bar').style.width = scrolled + '%';
+  });
+  
